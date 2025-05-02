686773ed56a4cf5980e41f767c11ef1b987c1b46
MaliAnand01 <anandsundesha@gmail.com>
2025-05-02 22:08:24 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// FAQ Accordion
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+
+        if (!groupHeader) return;
+
+        const group = groupHeader.parentElement;
+        groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        // Toggle icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // Toggle body visibility
+        groupBody.classList.toggle('open');
+
+        // Close other groups
+        const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+        otherGroups.forEach((otherGroup) => {
+            if (otherGroup !== group) {
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+                // Reset icon to plus
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+
+                // Close body
+                otherGroupBody.classList.remove('open');
+            }
+        });
+    });
+});
+
+// Mobile Menu
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+
+});
